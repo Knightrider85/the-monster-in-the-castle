76061e1e8520823318d62500ca161395dceaf539
danielvanpalo <daniel@grevensgastro.se>
2022-11-16 18:52:11 +0100

    16/11

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function loadBeerScene() {
     const button1 = document.getElementById("opt-1")
     const button2 = document.getElementById("opt-2")
 
-    text.textContent = "Du blickar över baren och ser att tjejen som jobbar i baren va assnygg, dags att beställa"
+    text.textContent = "Du blickar över baren och ser att tjejen som jobbar i baren va as snygg, dags att beställa"
     button1.textContent = "Kan jag få en bärs!"
     button2.textContent = "Skulle jag kunna beställa en öl?"
     button1.addEventListener("click", ()=> loadAngryScene());
@@ -65,7 +65,7 @@ function loadAngryScene() {
     text.textContent = "Du har gjort något som fått henne att sluta titta åt ditt håll, någon annan tar nu dina beställningar"
     button1.textContent = "Försök få hennes uppmärksamhet och kolla om hon kan rekommendera en ny öl at testa"
     button2.textContent = "Du beställer bara en stor stark från nya bartendern"
-    button1.addEventListener("click", ()=> loadHomeScene());
+    button1.addEventListener("click", ()=> loadHomeScene1());
     button2.addEventListener("click", ()=> loadCloserScene1());
 }
 //homescene
@@ -74,25 +74,13 @@ function loadHomeScene1() {
     const button1 = document.getElementById("opt-1")
     const button2 = document.getElementById("opt-2")
 
-    text.textContent = "Den snygge tjejen som jobbade i baren har slutat för dagen och följer med dig hem"
+    text.textContent = "Den snygge tjejen som jobbade i baren har slutat för dagen och tar ett glas med dig, sen följer med dig hem"
     button1.textContent = "Du bestämmer dig för att ge det en chans och försöka kyssa henne"
-    button2.textContent = "Du ernjuder henne något att dricka"
+    button2.textContent = "Du erbjuder henne något att dricka"
     button1.addEventListener("click", ()=> loadNotCleanedScene());
     button2.addEventListener("click", ()=> loadNotCleanedScene());
 }
 
-//not cleaned scene
-function loadHomeScene() {
-    const text = document.getElementById("text");
-    const button1 = document.getElementById("opt-1")
-    const button2 = document.getElementById("opt-2")
-
-    text.textContent = "Hon kände direkt när hon kom hem till dig att det kändes alldeles för ostädat, hon bestämde sig för att åka hem"
-    button1.textContent = "Du går tillbaka till krogen"
-    button2.textContent = "Du stannar"
-    button1.addEventListener("click", ()=> loadPubScene2());
-    button2.addEventListener("click", ()=> loadCloserScene1());
-}
 //pubscene 2
 function loadPubScene2() {
     const text = document.getElementById("text");
@@ -102,7 +90,7 @@ function loadPubScene2() {
     text.textContent = "Du kommer fram till krogen igen, vakterna tycker du är för berusad och släpper inte in dig"
     button1.textContent = "Du går tillbaka hem"
     button2.textContent = "Du håller inte med vakterna"
-    button1.addEventListener("click", ()=> loadCloserScene1());
+    button1.addEventListener("click", ()=> loadCloserScene2());
     button2.addEventListener("click", ()=> loadPoliceScene());
     //policescen
     function loadPoliceScene(){
@@ -113,25 +101,25 @@ function loadPubScene2() {
 //closer scene 1
 
 function loadCloserScene1() {
-    const text = document.getElementById("text");
-    text.textContent = "Du är hemma och lägger dig i soffan och äter upp chipsen du hade från igår, sen somnar du"
+    
+    alert ("Efter att fortsatt dricka ett par stor stark fick du till sist gå hem utan något napp ikväll, du bestämde dig för att testa igen imorgon men vara lite trevligare");
+
+
+}
+// closer scene 2
+function loadCloserScene1() {
+    
+    alert ("Du vingla sakta hem utan något napp ikväll, du bestämde dig för att testa igen imorgon men vara lite trevligare");
+
+
+}
+//not cleaned scene
+function loadNotCleanedScene() {
+    
+    alert ("Du hade inte städat hemma så hon tyckte det såg för ofräscht ut och valde att dricka upp sin öl du bjöd på och åka hem igen");
+
 
 }
 
 
 
-
-
-
-//val städa scen 1
-function loadCleanScene() {
-    const text = document.getElementById("text");
-    const button1 = document.getElementById("opt-1")
-    const button2 = document.getElementById("opt-2")
-
-    text.textContent = "Nu har du städat och duschat och redo för krogbesöket, du tar en taxi ner, och kommit fram"
-    button1.textContent = "Beställ en öl i baren"
-    button2.textContent = "Be om en meny och sätt dig i baren"
-    button1.addEventListener("click", ()=> loadBeerScene());
-    button2.addEventListener("click", ()=> loadMenuScene());
-}
